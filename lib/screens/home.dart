a923e3207ff63b00220fd0e8ac654364f9605747
Saman Ariyanpour <samanthelast@gmail.com>
2020-02-06 13:59:54 +0330

    working on big horizontal layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -26,7 +26,7 @@ class _HomeState extends State<Home> {
                 child: ListView(
               children:
                   snapshot.data.documents.map((DocumentSnapshot document) {
-                return Horizontal_big_card(document['title']);
+                return Horizontal_big_card(document['title'],document['creator']);
               }).toList(),
             ));
         }
--- lib/widgets/horizontal_big_card.dart
+++ lib/widgets/horizontal_big_card.dart
@@ -4,28 +4,28 @@ import 'package:online_courses/widgets/solo_video_layout.dart';
 
 /// This is the stateless widget that the main application instantiates.
 class Horizontal_big_card extends StatelessWidget {
-  final String description;
-final String imageVal = 'https://firebasestorage.googleapis.com/v0/b/online-course-27851.appspot.com/o/Capture001.png?alt=media&token=bc98902f-9419-4585-8c2f-b9a59b991fd9';
-  Horizontal_big_card(this.description);
-  String getDescription() {
-    return description;
+  final String title;
+  final String creator;
+  final String imageVal =
+      'https://firebasestorage.googleapis.com/v0/b/online-course-27851.appspot.com/o/Capture001.png?alt=media&token=bc98902f-9419-4585-8c2f-b9a59b991fd9';
+  Horizontal_big_card(this.title,this.creator);
+  String gettitle() {
+    return title;
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Column(
-        
         children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              
               ButtonBar(
                 alignment: MainAxisAlignment.end,
                 children: <Widget>[
                   FlatButton(
-                    child: const Text('همه'),
+                    child: const Text('جزئیات'),
                     onPressed: () {
                       _RouteToVideoListScreen(context);
                     },
@@ -33,44 +33,54 @@ final String imageVal = 'https://firebasestorage.googleapis.com/v0/b/online-cour
                 ],
               ),
               Container(
-               padding: EdgeInsets.only(right: 16),
-                child: Text(description,textDirection: TextDirection.rtl,style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                padding: EdgeInsets.only(right: 16, top: 16),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      title,
+                      textDirection: TextDirection.rtl,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      creator,
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
               ),
-
             ],
           ),
-            
           SingleChildScrollView(
             reverse: true,
             scrollDirection: Axis.horizontal,
             child: Row(
               children: <Widget>[
-                Solo_video_layout( '1001','اموزش  ریاضی 2','محمدرضا جوان',imageVal, '23','description' ),
-                 Solo_video_layout('1001',
-                 'openCV کتاب خانه', 'پانیز شگفت',imageVal, '23','description' ),
-                 Solo_video_layout('1001',
-                  'اموزش کراس','همایون بهشتی', imageVal, '23','description'),
-                 Solo_video_layout('1001',
-                 'کار با فوتوشاپ','محمد دیلمی', imageVal, '23','description' ),
-                 Solo_video_layout('1001',
-                 'اموزش c++','علی میرزازاده', imageVal, '23','description' ),
+                Solo_video_layout('1001', 'اموزش  ریاضی 2', 'محمدرضا جوان',
+                    imageVal, '23', 'description'),
+                Solo_video_layout('1001', 'openCV کتاب خانه', 'پانیز شگفت',
+                    imageVal, '23', 'description'),
+                Solo_video_layout('1001', 'اموزش کراس', 'همایون بهشتی',
+                    imageVal, '23', 'description'),
+                Solo_video_layout('1001', 'کار با فوتوشاپ', 'محمد دیلمی',
+                    imageVal, '23', 'description'),
+                Solo_video_layout('1001', 'اموزش c++', 'علی میرزازاده',
+                    imageVal, '23', 'description'),
               ],
             ),
-          ) 
+          )
         ],
       ),
     );
   }
+
   void _RouteToVideoListScreen(BuildContext context) {
-  
     Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) =>VideoList(
-            //categoryID
-          ),
+          builder: (context) => VideoList(
+              //categoryID
+              ),
         ));
-  
-
-}
+  }
 }
