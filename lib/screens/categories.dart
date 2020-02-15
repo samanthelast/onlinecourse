ebef5d50c8ec6f95ef616395742c8deec7e887ab
Saman Ariyanpour <samanthelast@gmail.com>
2020-02-15 13:39:59 +0330

    category ui done

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +16,12 @@ class Category extends StatefulWidget {
 class _CategoryState extends State<Category> {
   List<dynamic> getCategories = [];
   List<dynamic> disctinctCategories = [];
-  var unique ;
+  var unique;
 
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
+    return 
+    
+    StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('menu').snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
@@ -26,26 +29,69 @@ class _CategoryState extends State<Category> {
           case ConnectionState.waiting:
             return new Text('Loading...');
           default:
-
-            return Container(
-              padding: EdgeInsets.only(top: 32,right:16,left:16),
+            return Column(
+              
+              
+              children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 0,
                 child: ListView(
-                  
-              children:
-                  snapshot.data.documents.map((DocumentSnapshot document) {
-             getCategories.addAll(document['category']);
+                  children:
+                      snapshot.data.documents.map((DocumentSnapshot document) {
+                    getCategories.addAll(document['category']);
                     final seen = Set<String>();
-                     unique =
+                    unique =
                         getCategories.where((str) => seen.add(str)).toList();
 
-                    print(unique);
-return Text(unique.toString());
-
-              }).toList(),
-            ));
+                    
+                    return Text('');
+                  }).toList(),
+                ),
+              ),
+              Container(
+                alignment: Alignment.topRight,
+                padding: EdgeInsets.only(top: 32, left: 16, right: 16),
+                //width: MediaQuery.of(context).size.width,
+                //height:  400,
+                child: ListView.separated(
+                  itemCount: unique.length,
+                  shrinkWrap: true,
+                  
+                  itemBuilder: (BuildContext context, int index) {
+                    
+                    return  Directionality(
+                  child: ListTile(
+                    
+                    title: Text('${unique[index]}'),
+                    trailing: Icon(Icons.arrow_forward_ios,color: Colors.blue,),
+                    //leading: Icon(Icons.category,color: Colors.blue,),
+                    onTap: () {
+                       _RouteToVideoListScreen(context);
+                    },
+                  ),
+                  textDirection: TextDirection.rtl,
+                )
+                ;
+                  }, separatorBuilder: (context, index) {
+    return Divider();
+  }
+                  
+                ),
+              ),
+              
+            ]);
         }
       },
     );
+      
+    
+    
+    
+    
+    
+    
+    
   }
 
   void _RouteToVideoListScreen(BuildContext context) {
